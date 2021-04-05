cba59b92323de70a3860c268dd3c0f138cc579cf
Pedro Bala Simón <pbalasimon@gmail.com>
2021-04-05 23:50:01 +0200

    Anecdotes step 2 and 3

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,8 +1,19 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = (props) => {
-  const [selected, setSelected] = useState(0)
+const ANECDOTES = [
+  { title: 'If it hurts, do it more often', votes: 0 },
+  { title: 'Adding manpower to a late software project makes it later!', votes: 0 },
+  { title: 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.', votes: 0 },
+  { title: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', votes: 0 },
+  { title: 'Premature optimization is the root of all evil.', votes: 0 },
+  { title: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', votes: 0 }
+]
+
+const App = () => {
+
+  const [selected, setSelected] = useState(0);
+  const [anecdotes, setAnecdotes] = useState(ANECDOTES);
 
   function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -13,25 +24,34 @@ const App = (props) => {
     setSelected(randomNumber);
   }
 
+  const handleVote = () => {
+    console.log(ANECDOTES);
+    const newAnecdotes = [...anecdotes];
+    newAnecdotes[selected].votes += 1;
+    setAnecdotes(newAnecdotes);
+    console.log(ANECDOTES);
+  }
+
+  const getMaxVotes = () => {
+    const votes = anecdotes.map((anecdote) => anecdote.votes);
+    const maxVotesValue = Math.max(...votes);
+    const anecdoteMaxVoted = anecdotes.find(anecdote => anecdote.votes === maxVotesValue);
+    return anecdoteMaxVoted;
+  }
+
   return (
     <div>
-      {props.anecdotes[selected]}
+      <h3>{anecdotes[selected].title}</h3>
       <br />
+      <button onClick={handleVote}>Vote</button>
       <button onClick={handleClick}>Next anecdote</button>
+      <h5>Anecdote with most votes</h5>
+      {getMaxVotes().title}
     </div>
   )
 }
 
-const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
 ReactDOM.render(
-  <App anecdotes={anecdotes} />,
+  <App />,
   document.getElementById('root')
 )
